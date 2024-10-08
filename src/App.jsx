3938c0134d119a29e16bd41f5bb639190babec04
JunKyle <claudine.manrique@gmail.com>
2024-10-08 12:38:18 +0200

    design

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,28 @@ function App() {
   return (
     <>
       <div className="App">
-        <header>
-          <img
-            className="Home__image"
-            src="/images/cat-count-header.jpg"
-            alt="cat-count-header"
-          />
-        </header>
-        <section className="Home__section">
-          <h1 className="Home__h1">
-            Hello and welcome to the Cat Count Game App
-          </h1>
-          <p>Your cat-count counter is : 0 cat</p>
-          <h2>Your last cat-counted cats are :</h2>
-          <ul className="Home__list">
-            <li className="Home__item">Cat-count the</li>
-          </ul>
-          <button className="confirm">
-            <a href="./addCat.html">Add a cat</a>
-          </button>
-        </section>
+        <div className="Home">
+          <header className="Home__header">
+            <img
+              className="Home__image"
+              src="/images/cat-count-header.jpg"
+              alt="cat-count-header"
+            />
+          </header>
+          <section className="Home__section">
+            <h1 className="Home__h1">
+              Hello and welcome to the Cat Count Game App
+            </h1>
+            <p>Your cat-count counter is : 0 cat</p>
+            <h2>Your last cat-counted cats are :</h2>
+            <ul className="Home__list">
+              <li className="Home__item">Cat-count the</li>
+            </ul>
+            <button className="confirm">
+              <a href="./addCat.html">Add a cat</a>
+            </button>
+          </section>
+        </div>
       </div>
     </>
   );
